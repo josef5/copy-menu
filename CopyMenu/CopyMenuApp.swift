c2df37e23347481ad77935c57803a6156f73f643
Jose Espejo <joseespejo@yahoo.com>
2024-10-07 19:22:25 +0100

    Initial Commit

---
+++ CopyMenu/CopyMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  CopyMenuApp.swift
+//  CopyMenu
+//
+//  Created by Jose Espejo on 07/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct CopyMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
