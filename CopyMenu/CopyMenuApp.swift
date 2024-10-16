961753c1ae0024a45379eb02d85d9eb017197ec8
Jose Espejo <joseespejo@yahoo.com>
2024-10-16 20:16:56 +0100

    feat: use WindowAccessor to set the level of the edit window to .floating

--- CopyMenu/CopyMenuApp.swift
+++ CopyMenu/CopyMenuApp.swift
@@ -12,20 +12,25 @@ let EDIT_WINDOW_ID = "edit-window"
 @main
 struct CopyMenuApp: App {
     @StateObject private var viewModel = ViewModel()
+    @State var window: NSWindow?
     @Environment(\.dismissWindow) private var dismissWindow
-    
+
     var body: some Scene {
         MenuBarExtra("Menu", systemImage: "clipboard") {
             MenuBarView(viewModel: viewModel)
         }
         .menuBarExtraStyle(.menu)
-        
+
         Window("Edit Menu", id: EDIT_WINDOW_ID) {
             EditView(viewModel: viewModel)
                 .frame(minWidth: 350, minHeight: max(CGFloat(viewModel.menuItems.count) * 50, 300))
+                .background(WindowAccessor(window: $window))
+                .onChange(of: window) { _, newWindow in
+                    newWindow?.level = .floating
+                }
         }
         .windowResizability(.contentSize)
         .defaultPosition(.center)
-//        .windowLevel(.floating)
+//        .windowLevel(.floating) // TODO: implement in macOS 15
     }
 }
